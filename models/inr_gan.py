a272f4d21a4b62fa33ccb8638019ee1bf02ee948
quanzhou li <qzdy369@vip.qq.com>
2022-05-30 15:16:22 -0700

    update

--- models/inr_gan.py
+++ models/inr_gan.py
@@ -88,7 +88,7 @@ class INRGenerator(nn.Module):
 
         self.mapping_network = nn.Sequential(
             *[INRGeneratorBlock(dims[i], dims[i + 1], True, is_first_layer=(i == 0)) for i in range(len(dims) - 2)])
-        self.connector = ResBlock(dims[-2], dims[-1], n_neurons=5*generator_hid_dim)
+        self.connector = ResBlock(dims[-2], dims[-1], n_neurons=4*generator_hid_dim)
 
     def forward(self, img: Tensor, first_frame: Tensor, last_frame: Tensor, width: int, height: int) -> Dict[
         str, Union[Union[Tensor, float], Any]]:
