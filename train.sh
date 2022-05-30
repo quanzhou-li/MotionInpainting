8369c8ace6042037295c73e9273e9fbff8cc1683
quanzhou li <qzdy369@vip.qq.com>
2022-05-29 19:29:11 -0700

    update

--- train.sh
+++ train.sh
@@ -3,4 +3,5 @@
 python train.py --work-dir logs \
        --data-path datasets_parsed_motion_imgs/ \
        --inr-config config/inr-gan.yml \
-       --batch-size 12
+       --batch-size 12 \
+       --lr 1e-3
