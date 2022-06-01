011822aee70138c171cb0681f5e190cef6430a36
quanzhou li <qzdy369@vip.qq.com>
2022-06-01 16:18:02 -0700

    add root parameters for training

--- models/inr_gan.py
+++ models/inr_gan.py
@@ -62,12 +62,12 @@ class INRGenerator(nn.Module):
         self.inr = FourierINRs(self.config)
         # self.inr = FourierINRs(self.config)
 
-        self.frame_D = 330
+        self.frame_D = 333
         self.latent_D = 512
         self.fframe_enc = ResBlock(self.frame_D, self.latent_D)
         self.lframe_enc = ResBlock(self.frame_D, self.latent_D)
 
-        self.width, self.height = 64, 330
+        self.width, self.height = 64, self.frame_D
         self.img_enc = ResBlock(self.width * self.height, self.latent_D)
         self.enc_mu = nn.Linear(self.latent_D, self.latent_D)
         self.enc_var = nn.Linear(self.latent_D, self.latent_D)
