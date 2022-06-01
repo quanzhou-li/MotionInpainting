0129a2b7c2de04e244cd68e5085b0f541dc6ae0a
quanzhou li <qzdy369@vip.qq.com>
2022-06-01 13:38:29 -0700

    update

--- train.sh
+++ train.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-python train.py --work-dir logs \
+echo Enter the path to logs
+# shellcheck disable=SC2162
+read -p 'Path: ' logs
+
+python train.py --work-dir $logs \
        --data-path datasets_parsed_motion_imgs/ \
        --inr-config config/inr-gan.yml \
-       --batch-size 12 \
+       --batch-size 16 \
+       --use-multigpu true \
+       --n-workers 8 \
        --lr 1e-3
