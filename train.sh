5b0ed27de2833843b6012bc06d75969b28d6fdc0
quanzhou li <qzdy369@vip.qq.com>
2022-06-01 16:33:22 -0700

    update

--- train.sh
+++ train.sh
@@ -5,9 +5,10 @@ echo Enter the path to logs
 read -p 'Path: ' logs
 
 python train.py --work-dir $logs \
-       --data-path datasets_parsed_motion_imgs/ \
+       --data-path datasets_parsed_motion_inpaint_64frames/ \
        --inr-config config/inr-gan.yml \
        --batch-size 16 \
        --use-multigpu true \
-       --n-workers 8 \
-       --lr 1e-3
+       --n-workers 12 \
+       --n-epochs 500 \
+       --lr 5e-4
