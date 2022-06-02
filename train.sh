dad197a246e3dbf4258d9d3e4abfb63875548cbd
quanzhou li <qzdy369@vip.qq.com>
2022-06-01 18:02:14 -0700

    reverse

--- train.sh
+++ train.sh
@@ -4,5 +4,6 @@ python train.py --work-dir logs \
        --data-path datasets_parsed_motion_imgs/ \
        --inr-config config/inr-gan.yml \
        --batch-size 16 \
+       --n-workers 8 \
        --use-multigpu true \
        --lr 1e-3
