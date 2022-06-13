694f699c95a27a6c05575ae0eea61f6997f2e01e
quanzhou li <qzdy369@vip.qq.com>
2022-06-13 11:51:05 -0700

    update train.sh

--- train.sh
+++ train.sh
@@ -9,6 +9,6 @@ python train.py --work-dir $logs \
        --inr-config config/inr-gan.yml \
        --batch-size 16 \
        --use-multigpu false \
-       --n-workers 1 \
+       --n-workers 12 \
        --n-epochs 300 \
        --lr 5e-4
