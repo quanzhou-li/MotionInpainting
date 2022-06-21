b2b76be1be200d2de669f5a4b83b6fb1b55cc01a
quanzhou li <qzdy369@vip.qq.com>
2022-06-20 21:27:50 -0700

    change dimension to 342

--- train/trainer.py
+++ train/trainer.py
@@ -168,16 +168,16 @@ class Trainer:
         loss_tv_pose = self.compute_variation_Loss(drec['imgs'].view(bs, height, width)[:, :330, :], tv_weight)
 
         # loss_root = 100 * self.LossL2(data['motion_imgs'][:, 330:333, :], drec['imgs'].view(bs, height, width)[:, 330:333, :])
-        # loss_root = 30 * self.LossL1(data['motion_imgs'][:, 330:333, :], drec['imgs'].view(bs, height, width)[:, 330:333, :])
+        loss_root = 30 * self.LossL1(data['motion_imgs'][:, 330:333, :], drec['imgs'].view(bs, height, width)[:, 330:333, :])
         # loss_tv_root = self.compute_variation_Loss(drec['imgs'].view(bs, height, width)[:, 330:333, :], tv_weight)
 
-        # loss_obj_orient = 100 * self.LossL2(data['motion_imgs'][:, 333:339, :], drec['imgs'].view(bs, height, width)[:, 333:339, :])
+        loss_obj_orient = 100 * self.LossL2(data['motion_imgs'][:, 333:339, :], drec['imgs'].view(bs, height, width)[:, 333:339, :])
         # loss_obj_orient = self.compute_geodesic_loss(data['motion_imgs'][:, 333:339, :].permute(0, 2, 1),
         #                                                    drec['imgs'].view(bs, height, width)[:, 333:339, :].permute(0, 2, 1))
         # loss_tv_obj_orient = self.compute_variation_Loss(drec['imgs'].view(bs, height, width)[:, 333:339, :], tv_weight)
 
         # loss_obj_transl = 100 * self.LossL2(data['motion_imgs'][:, 339:, :], drec['imgs'].view(bs, height, width)[:, 339:, :])
-        # loss_obj_transl = 30 * self.LossL1(data['motion_imgs'][:, 339:, :], drec['imgs'].view(bs, height, width)[:, 339:, :])
+        loss_obj_transl = 30 * self.LossL1(data['motion_imgs'][:, 339:, :], drec['imgs'].view(bs, height, width)[:, 339:, :])
         # loss_tv_obj_transl = self.compute_variation_Loss(drec['imgs'].view(bs, height, width)[:, 339:, :], tv_weight)
 
         q_z = torch.distributions.normal.Normal(drec['mean'], drec['std'])
@@ -197,9 +197,9 @@ class Trainer:
             'loss_kl': loss_kl,
             # 'loss_firstframe': loss_firstframe,
             # 'loss_lastframe': loss_lastframe,
-            # 'loss_root': loss_root,
-            # 'loss_obj_orient': loss_obj_orient,
-            # 'loss_obj_transl': loss_obj_transl,
+            'loss_root': loss_root,
+            'loss_obj_orient': loss_obj_orient,
+            'loss_obj_transl': loss_obj_transl,
             # 'loss_tv_pose': loss_tv_pose,
             # 'loss_tv_root': loss_tv_root,
             # 'loss_tv_obj_orient': loss_tv_obj_orient,
