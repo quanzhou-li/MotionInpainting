c78ce08ec82fc58ea747b665e8e9d94249d1c029
quanzhou li <qzdy369@vip.qq.com>
2022-07-13 16:48:26 -0700

    fourier5, se factorized

--- models/inrs/inrs.py
+++ models/inrs/inrs.py
@@ -207,7 +207,7 @@ class FourierINRs(INRs):
                 input_dim,
                 layer_sizes[i+1],
                 # layer_type='mm_se_factorized')
-                layer_type='factorized')
+                layer_type='se_factorized')
             curr_transform_layers.append(INRProxy(create_activation('relu')))
 
             hid_layers.append((INRSequential(*curr_transform_layers)))
