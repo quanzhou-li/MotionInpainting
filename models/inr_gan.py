514ef7dfc485dc9e266c1116367189c1b6f9301c
quanzhou li <qzdy369@vip.qq.com>
2022-07-25 15:04:14 -0700

    change dimension to 342

--- models/inr_gan.py
+++ models/inr_gan.py
@@ -63,7 +63,7 @@ class INRGenerator(nn.Module):
         # self.inr = FourierINRs(self.config)
         self.dist = None
 
-        self.frame_D = 330
+        self.frame_D = 342
         self.latent_D = 512
         self.dim_z = 1024
         self.fframe_enc = ResBlock(self.frame_D, self.latent_D)
